3d5c832bdb7baacebc614ecaf857f8b250074f69
Christopher Gammie <contact@gammie.co.uk>
2015-04-15 15:58:20 +0100

    Add support for disabled class.

--- addon/components/active-link.js
+++ addon/components/active-link.js
@@ -2,8 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNameBindings: ['active'],
+  disabled: false,
+  classNameBindings: ['active', 'disabled'],
   active: function() {
+
+    var disabled = this.get('disabled');
+
+    if (disabled) {
+      return false;
+    }
+
     return this.get('childViews').anyBy('active');
-  }.property('childViews.@each.active')
+  }.property('childViews.@each.active', 'disabled')
 });
