0108dcd71673f32ad10d43577f0518fa7ec2d774
Alex Speller <alex@alexspeller.com>
2015-09-09 03:06:02 +0100

    Update for latest ember

--- addon/components/active-link.js
+++ addon/components/active-link.js
@@ -2,8 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
+  childLinkViews: [],
   classNameBindings: ['active'],
-  active: function() {
-    return this.get('childViews').anyBy('active');
-  }.property('childViews.@each.active')
+
+  active: Ember.computed('childLinkViews.@each.active', function() {
+    return Ember.A(this.get('childLinkViews')).isAny('active');
+  }),
+
+  didRender: function() {
+    Ember.run.schedule('afterRender', this, function() {
+      var childLinkElements = this.$('a.ember-view');
+
+      var childLinkViews = childLinkElements.toArray().map(view =>
+        this._viewRegistry[view.id]
+      );
+
+      this.set('childLinkViews', childLinkViews);
+    });
+  }
+
 });
